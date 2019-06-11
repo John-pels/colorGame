38c2868411ebe00c9b9987cf796167bd1bd3dea6
john-pels <oluseyiajeigbe@yahoo.com>
2019-06-11 11:31:40 +0100

    Adding Js code that gets random colors

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.querySelector("#colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 colorDisplay.textContent = pickedColor;
 for (var i = 0; i < squares.length; i++) {
     //add initial colors to squares
@@ -11,10 +12,12 @@ for (var i = 0; i < squares.length; i++) {
     squares[i].addEventListener("click", function() {
         //grab the color of the clicked square
         var clickedColor = this.style.backgroundColor;
+        console.log(clickedColor, pickedColor);
         //compare color to pickedColor
         if (clickedColor === pickedColor) {
             messageDisplay.textContent = "Correct";
             changeColors(clickedColor);
+            h1.style.backgroundColor = clickedColor;
         } else {
             this.style.backgroundColor = "#232323";
             messageDisplay.textContent = "Try Again!";
@@ -42,7 +45,7 @@ function generateRandomColors(num) {
     //add num random colors to array
     for (var i = 0; i < num; i++) {
         // get random color and push into array
-
+        arr.push(randomColor());
     }
     //return that array
     return arr;
@@ -56,5 +59,5 @@ function randomColor() {
 
     //pick a blue from 0-255
     var b = Math.floor(Math.random() * 256);
-
+    return "rgb(" + r + ", " + g + ", " + b + ")";
 }
