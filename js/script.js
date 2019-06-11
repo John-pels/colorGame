5ccdd71faa106d477ca96a0adce4c7f070605cb0
john-pels <oluseyiajeigbe@yahoo.com>
2019-06-11 11:12:06 +0100

    Adding Js for the interactiveness

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-var colors = ["rgb(255, 0, 0)", "rgb(255, 255, 0)", "rgb(0, 255, 0)", "rgb(0, 255, 255)", "rgb(0, 0, 255)", "rgb(255, 0, 255)"];
+var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.querySelector("#colorDisplay");
+var messageDisplay = document.querySelector("#message");
 colorDisplay.textContent = pickedColor;
 for (var i = 0; i < squares.length; i++) {
     //add initial colors to squares
@@ -12,9 +13,48 @@ for (var i = 0; i < squares.length; i++) {
         var clickedColor = this.style.backgroundColor;
         //compare color to pickedColor
         if (clickedColor === pickedColor) {
-            alert("Correct!");
+            messageDisplay.textContent = "Correct";
+            changeColors(clickedColor);
         } else {
             this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "Try Again!";
         }
     });
+}
+
+function changeColors(color) {
+    //loop through all squares
+    for (var i = 0; i < squares.length; i++) {
+        //change each color to match given color
+        squares[i].style.backgroundColor = color;
+    }
+}
+//function that changes the color of the boxes when a new game button is clicked
+function pickColor() {
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
+}
+
+//function that generates random colors
+function generateRandomColors(num) {
+    //make an array
+    var arr = [];
+    //add num random colors to array
+    for (var i = 0; i < num; i++) {
+        // get random color and push into array
+
+    }
+    //return that array
+    return arr;
+}
+
+function randomColor() {
+    //pick a red from 0-255
+    var r = Math.floor(Math.random() * 256);
+    //pick a green from 0-255
+    var g = Math.floor(Math.random() * 256);
+
+    //pick a blue from 0-255
+    var b = Math.floor(Math.random() * 256);
+
 }
